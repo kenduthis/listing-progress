3b7bdbdde010435124a467fdf466841ea73189c3
kenduthis <kenduthis@gmail.com>
2024-07-31 01:06:02 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
 app.use(express.static('public'));
 
-app.get('/api/balances', (req, res) => {
+app.get('/public/balances', (req, res) => {
   const filePath = path.join(__dirname, 'balances.json');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
